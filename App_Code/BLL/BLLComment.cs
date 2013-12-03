0bb76ad17ad071b03251d5b8eaae6ad78b3a29e3
Veerle Claes <claes.veerle@skynet.be>
2013-12-03 14:30:11 +0100

    CommentVeld toegevoegd

---
+++ App_Code/BLL/BLLComment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for BLLComment
+/// </summary>
+public class BLLComment
+{
+    DALComment DALComments = new DALComment();
+
+    public void insert(Comment c_ev)
+    {
+        DALComments.insert(c_ev);
+    }
+
+    public List<Comment> selectAll(int comment_id)
+    {
+        return DALComments.selectAll(comment_id);
+    }
+}
---
+++ App_Code/DAL/DALComment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for DALComment
+/// </summary>
+public class DALComment
+{
+    private CreativityEventDataContext dc = new CreativityEventDataContext();
+
+    public void insert(Comment c_ev)
+    {
+        dc.Comments.InsertOnSubmit(c_ev);
+        dc.SubmitChanges();
+    }
+
+    public List<Comment> selectAll(int comment_id)
+    {
+        var query = (from u in dc.Comments where u.Id == comment_id select u).ToList();
+        return query;
+    }
+}
